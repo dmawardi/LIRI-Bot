ccf0b5bb208909ce6261fb770a070e0a0754f18e
Dannar Mawardi <30311230+dmawardi@users.noreply.github.com>
2019-08-19 16:53:36 +1000

    Update bands.js

---
+++ bands.js
@@ -0,0 +1,13 @@
+var app_id = '';
+var artistName = '';
+var date = ["upcoming", "past", "all"];
+
+// Expected URL
+// 'https://rest.bandsintown.com/artists/{artistname}/events'
+
+var baseURL = 'https://rest.bandsintown.com/artists/'+{artistname: artistName}+'/events';
+
+
+function queryBuilder() {
+  
+}
