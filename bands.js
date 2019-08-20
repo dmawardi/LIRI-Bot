3bca5501378dd575774f8157575dd49582430aea
Dannar Mawardi <dmawardi@gmail.com>
2019-08-20 22:47:08 +1000

    added git ignore

--- bands.js
+++ bands.js
@@ -5,7 +5,7 @@ var date = ["upcoming", "past", "all"];
 // Expected URL
 // 'https://rest.bandsintown.com/artists/{artistname}/events'
 
-var baseURL = 'https://rest.bandsintown.com/artists/'+{artistname: artistName}+'/events';
+var baseURL = 'https://rest.bandsintown.com/artists/'+{artistname: artistName}+'/events?date='+date[0];
 
 
 function queryBuilder() {
